26161c58932d3c7514de1b8bfe33bc9ca6546414
yuorei <yuorei71@gmail.com>
2023-07-14 21:57:36 +0900

    JWT生成

---
+++ jwt.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func JwtGenerate() (string, error) {
+	// Claimsオブジェクトの作成
+	claims := jwt.MapClaims{
+		"user_id": 1,// TODO userIdを入れるようにする
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	// ヘッダーとペイロードの生成
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// トークンに署名を付与
+	tokenString, err := token.SignedString([]byte("SECRET_KEY"))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
