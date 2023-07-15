97f2533bbcb5f5d951dd08463ff4bdf9ac14b281
yuorei <yuorei71@gmail.com>
2023-07-15 10:24:00 +0900

    各種呼び出し

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"tippers-back/db"
+	"tippers-back/middleware"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("読み込み出来ませんでした: %v", err)
+	}
 	r := gin.Default()
+	db.NewDB()
+	r.Use(middleware.AuthMiddleware())
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
